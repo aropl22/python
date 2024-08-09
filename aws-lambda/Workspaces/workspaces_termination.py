ee036ffffc399290160c63bdcee8c5af86f88bda
Arek <arobrooklyn@gmail.com>
2024-08-08 21:37:47 -0400

    updated

--- aws-lambda/Workspaces/workspaces_termination.py
+++ aws-lambda/Workspaces/workspaces_termination.py
@@ -21,7 +21,6 @@ def lambda_handler(event, context):
         ]
         )
         print('Terminating workspace '+workspacesIDs['Workspaces'][i]['WorkspaceId'])
-        print(TerminateWorkspaceRequests)
         
 if __name__ == "__main__":
         lambda_handler({}, {})
